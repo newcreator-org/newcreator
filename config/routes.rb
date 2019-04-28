c02bda9a02e512cbbe1f2662ba8409739335c206
Yuki Koyama <koyama@stf.hack.or.jp>
2019-04-28 10:54:18 +0930

    Imported base UI for public lp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'top/index'
+  root to: 'public#index'
+  get 'public/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
