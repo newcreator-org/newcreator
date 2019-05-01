0198728666cc603d9842108fbdf74c49a3dfb4f5
Yuki Koyama <koyama@stf.hack.or.jp>
2019-05-01 16:49:04 +0930

    デザインの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get 'textbook/index'
   get 'textbook/show'
-  get 'help/index'
-  get 'top/index'
+  get 'dashboard' => 'top#index'
+  get 'help' => 'help#index'
   root to: 'public#index'
-  get 'public/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
