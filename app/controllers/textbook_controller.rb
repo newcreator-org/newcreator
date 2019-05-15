ccc2839e4e58e8871084a0e88efe4548caf417a4
Yuki Koyama <koyama@stf.hack.or.jp>
2019-05-16 05:15:30 +0930

    テキストを表示する方法を作成

--- app/controllers/textbook_controller.rb
+++ app/controllers/textbook_controller.rb
@@ -1,7 +1,9 @@
 class TextbookController < ApplicationController
   def index
+    @textbooks = Textbook.all
   end
 
   def show
+    @textbook = Textbook.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  get 'public/index'
+
+  unauthenticated :user do
+    root :to => "public#index"
+  end
+
+  # ログインしようとしたユーザーをリダイレクト
+  get '/users', to: redirect("/users/sign_up")
+
+  devise_for :users, :controllers => {
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions'
+  }
+
+
   get 'textbook/index'
   get 'textbook/show'
+  resources :textbook, param: :id
+
   get 'dashboard' => 'top#index'
   get 'help' => 'help#index'
+
   root to: 'public#index'
+
 end
